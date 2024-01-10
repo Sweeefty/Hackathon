255795ee54e75e7ec0ff51b5c9e53d73ada29386
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2024-01-10 08:36:04 +0100

    Adding connexion Page

--- hackathon2024/src/app/page.tsx
+++ hackathon2024/src/app/page.tsx
@@ -1,13 +1,31 @@
-import { Button, ChakraProvider } from '@chakra-ui/react'
+import { Button, ChakraProvider, Center, Box, Flex, Heading, Input, Stack } from '@chakra-ui/react'
 import Image from 'next/image'
 import { Modal } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <ChakraProvider>
-        <Button className='w-full h-full bg-white'>
-
-        </Button>
+      <Flex
+      align="center"
+      justify="center"
+      h="100vh"
+      bg="white" 
+    >
+      <Box p={12} borderWidth={1} borderRadius={12} boxShadow="lg" bg="#70DECF">
+        <Heading as="h2" size="lg" mb={4}>
+          Connexion
+        </Heading>
+        <Stack spacing={16}>
+          <Stack spacing={6}>
+            <Input placeholder='Email' size='sm' bg='white' />
+            <Input placeholder='Mot de Passe' size='sm' bg='white' />
+          </Stack>
+          <Button bg='#666666' size="md">
+            Connexion
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
       
     </ChakraProvider>
   )
