09f5043d3704f3b96364979deb99f332aae5e48d
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2024-01-11 10:04:09 +0100

    fixing connexion page + add logo

--- hackathon2024/src/app/page.tsx
+++ hackathon2024/src/app/page.tsx
@@ -11,14 +11,18 @@ export default function Home() {
       h="100vh"
       bg="white" 
     >
-      <Box p={12} borderWidth={1} borderRadius={12} boxShadow="lg" bg="#70DECF">
-        <Heading as="h2" size="lg" mb={4}>
+
+      <Box position="absolute" top="4" left="4">
+          <Image src="/logo.png" alt="Your Image" width={300} height={300} />
+        </Box>
+      <Box p={28} borderWidth={1} borderRadius={12} boxShadow="lg" bg="#70DECF">
+        <Heading as="h2" size="lg" mb={16}>
           Connexion
         </Heading>
         <Stack spacing={16}>
           <Stack spacing={6}>
-            <Input placeholder='Email' size='sm' bg='white' />
-            <Input placeholder='Mot de Passe' size='sm' bg='white' />
+            <Input placeholder='Email' size='sm' bg='#666666' />
+            <Input placeholder='Mot de Passe' size='sm' bg='#666666' />
           </Stack>
           <Button bg='#666666' size="md">
             Connexion
