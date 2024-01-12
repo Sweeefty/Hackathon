7c3092e49306d1eca2b1793fe43625df669dd3a0
Sweeefty <108686673+Sweeefty@users.noreply.github.com>
2024-01-12 09:42:32 +0100

    Rename Calendrier page to Apropos and commit the home page

---
+++ hackathon2024/src/app/Apropos/page.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "@/components/layout"
+
+export default function Apropos(){
+    return(
+        <Layout>
+        <div>
+        <h1>À propos du BDE</h1>
+        </div>
+        </Layout>
+    )
+}
